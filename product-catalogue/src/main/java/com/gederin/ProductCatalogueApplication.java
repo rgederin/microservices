5f1fcb2ca8cabc2f3c64afd9b775bbd7a3953285
rgederin <ruslan.gederin@goeuro.com>
2017-12-11 11:53:41 +0200

    add eureka configuration for clients

--- product-catalogue/src/main/java/com/gederin/ProductCatalogueApplication.java
+++ product-catalogue/src/main/java/com/gederin/ProductCatalogueApplication.java
@@ -2,8 +2,10 @@ package com.gederin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductCatalogueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCatalogueApplication.class, args);
